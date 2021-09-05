bfef7ad383e5d5ca968775ceb10be7dffc5c1dab
gleatherwood <79239593+gleatherwood@users.noreply.github.com>
2021-09-05 17:07:48 -0400

    update

---
+++ resources/[custom]/input/__resource.lua
@@ -0,0 +1,22 @@
+-- Leaked By: Leaking Hub | J. Snow | leakinghub.com
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+-- Leaked By: Leaking Hub | J. Snow | leakinghub.com
+
+ui_page 'html/index.html'
+
+-- Leaked By: Leaking Hub | J. Snow | leakinghub.com
+
+client_scripts {
+  'client/client.lua',
+}
+
+-- Leaked By: Leaking Hub | J. Snow | leakinghub.com
+
+file {
+  'html/index.html',
+  'html/pricedown.black.ttf',
+}
+
+-- Leaked By: Leaking Hub | J. Snow | leakinghub.com
